2c124f1be04c1d2e4acc54e59caae50269285830
pavelshikin <sh1k@bk.ru>
2021-07-30 19:12:18 +0300

    1 file modified

--- src/utilits/api.js
+++ src/utilits/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
    baseURL: 'https://techno-train.herokuapp.com/',
    withCredentials: true,
+   credentials: 'same-origin', 
    headers: {
       'Accept': 'application/json',
       'Access-Control-Allow-Origin': '*',
