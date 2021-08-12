c56961d7c22e193a9a93f4a39bf4c3c2c373baa1
pavelshikin <sh1k@bk.ru>
2021-08-12 18:14:03 +0300

    1 file modified

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllPosts } from '../store/actions/postActions';
-import { postsByCategory } from '../utilits/postsByCategory';
+import { postsByCategoryId } from '../utilits/postsByCategory';
 import NoteForm from '../components/Note/NoteForm';
 import NoteList from '../components/Note/NoteList';
 
 const ProductsPage = () => {
   const posts = useSelector(state => state.posts.allPosts);
   const dispatch = useDispatch();
-  let products = postsByCategory(posts, 'products');
+  let products = postsByCategoryId(posts, 'products');
 
   useEffect(() => {
     dispatch(fetchAllPosts());
