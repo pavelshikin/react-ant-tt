42c4082642accc993e84b9d589aa430720e64afc
pavelshikin <sh1k@bk.ru>
2021-08-13 10:09:21 +0300

    1 file modified

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useAuth } from '../contexts/AuthContext';
 import { fetchPosts } from '../store/actions/postActions';
 import { postsByCategory } from '../utilits/postsByCategory';
 import NoteForm from '../components/Note/NoteForm';
 import NoteList from '../components/Note/NoteList';
 
 const NotesPage = () => {
+  const { user } = useAuth();
   const posts = useSelector(state => state.posts.posts);
   const dispatch = useDispatch();
   let notes = postsByCategory(posts, 'notes');
 
   useEffect(() => {
+    console.log(user.roles);
     dispatch(fetchPosts());
   }, [dispatch]);
 
