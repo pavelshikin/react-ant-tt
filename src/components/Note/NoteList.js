c32560593688d63143502a4b5e54ce9d54407acc
pavelshikin <sh1k@bk.ru>
2021-08-14 02:00:05 +0300

    3 files modified

--- src/components/Note/NoteList.js
+++ src/components/Note/NoteList.js
@@ -11,26 +11,42 @@ const NoteList = ({ notes, allPosts, checkBox }) => {
     setCheck(prevState => !prevState);
   };
 
+  const renderCheck = () => {
+    return (
+      <>
+        {user && user.roles.indexOf('OWNER' || 'ADMIN') !== -1 && checkBox ? (
+          <div style={{ overflow: 'auto' }}>
+            <Checkbox
+              onChange={onChange}
+              checked={check}
+              className={s.checkBox}
+            >
+              Все
+            </Checkbox>
+          </div>
+        ) : (
+          ''
+        )}
+      </>
+    );
+  };
+
   const renderItems = posts => {
     return posts.map((post, idx) => <NoteItem key={idx} note={post} />);
   };
 
   if (notes.length === 0) {
-    return <h2 className={'t-center'}>Нет заметок</h2>;
+    return (
+      <>
+        {renderCheck()}
+        <h2 className={'t-center'}>Нет заметок</h2>
+      </>
+    );
   }
 
   return (
     <div className={s.content}>
-      {user && user.roles.indexOf('OWNER' || 'ADMIN') !== -1 && checkBox ? (
-        <div style={{ overflow: 'auto' }}>
-          <Checkbox onChange={onChange} checked={check} className={s.checkBox}>
-            Все
-          </Checkbox>
-        </div>
-      ) : (
-        ''
-      )}
-
+      {renderCheck()}
       {check ? renderItems(allPosts) : renderItems(notes)}
     </div>
   );
