97c9b3014a29fe026539b7b9d5f9a2e33487ed66
pavelshikin <sh1k@bk.ru>
2021-09-05 20:07:10 +0300

    1 file modified

--- src/utilits/api.js
+++ src/utilits/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://techno-train.herokuapp.com/',
   withCredentials: true,
   credentials: 'include',
   headers: {
