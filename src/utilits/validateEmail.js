e1a236226c57b641afabaebe0e600b51f04ed379
pavelshikin <sh1k@bk.ru>
2021-09-08 12:33:52 +0300

    1 file deleted

--- src/utilits/validateEmail.js
+++ src/utilits/validateEmail.js
@@ -2,4 +2,4 @@ export const validateEmail = email => {
   const re = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
   return re.test(String(email).toLowerCase());
 };
-let a;
+
